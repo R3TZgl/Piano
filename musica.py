9efbcb55b6851c779a26d844e899c3af28b6ed68
Gabriel Retzlaff <gretzlaff13@gmail.com>
2022-05-15 19:08:09 -0300

    Correção da nota B

--- musica.py
+++ musica.py
@@ -4,6 +4,7 @@ from time import sleep
 
 
 def som(a):
+    pygame.display.update()
     pygame.mixer.music.load(a)
     pygame.mixer.music.play(1)
     sleep(0.5)
--- piano.py
+++ piano.py
@@ -55,33 +55,50 @@ while True:
     comand = pygame.key.get_pressed()
     if comand[pygame.K_z]:
         pygame.draw.rect(tela, branco, (25, 25, l_tecla, a_tecla))
+        pygame.draw.rect(tela, preto, (25, 25, l_tecla, a_tecla),2)
         som("Notas/do.wav")
     
     if comand[pygame.K_x]:
         pygame.draw.rect(tela, branco, (25 + l_tecla, 25, l_tecla, a_tecla))
+        pygame.draw.rect(tela, preto, (25 + l_tecla, 25, l_tecla, a_tecla),2)
         som("Notas/re.wav")
         
     if comand[pygame.K_c]:
         pygame.draw.rect(tela, branco, (25 + l_tecla * 2, 25, l_tecla, a_tecla))
+        pygame.draw.rect(tela, preto, (25 + l_tecla * 2, 25, l_tecla, a_tecla),2)
         som("Notas/mi.wav")
     
     if comand[pygame.K_v]:
         pygame.draw.rect(tela, branco, (25 + l_tecla * 3, 25, l_tecla, a_tecla))
+        pygame.draw.rect(tela, preto, (25 + l_tecla * 3, 25, l_tecla, a_tecla),2)
         som("Notas/fa.wav")
     
     if comand[pygame.K_b]:
         pygame.draw.rect(tela, branco, (25 + l_tecla * 4, 25, l_tecla, a_tecla))
+        pygame.draw.rect(tela, preto, (25 + l_tecla * 4, 25, l_tecla, a_tecla),2)
         som("Notas/sol.wav")
     
     if comand[pygame.K_n]:
         pygame.draw.rect(tela, branco, (25 + l_tecla * 5, 25, l_tecla, a_tecla))
+        pygame.draw.rect(tela, preto, (25 + l_tecla * 5, 25, l_tecla, a_tecla),2)
         som("Notas/la.wav")
 
     
     if comand[pygame.K_m]:
         pygame.draw.rect(tela, branco, (25 + l_tecla * 6, 25, l_tecla, a_tecla))
-        som("Notas/si.wav")
+        pygame.draw.rect(tela, preto, (25 + l_tecla * 6, 25, l_tecla, a_tecla),2)
+        som("Notas/si_fade.wav")
 
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     pygame.display.update()
